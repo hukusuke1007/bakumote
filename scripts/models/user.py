06ae1b0666dae206c6110e7abef39240c4220c93
Shohei Nakagawa <hukusuke1007@gmail.com>
2020-09-30 10:27:20 +0900

    added users script

---
+++ scripts/models/user.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, name, description, genderId, prefectureId, imageName, hobby, favoriteType):
+        self.name = name
+        self.description = description
+        self.genderId = genderId
+        self.prefectureId = prefectureId
+        self.imageName = imageName
+        self.hobby = hobby
+        self.favoriteType = favoriteType
+        
