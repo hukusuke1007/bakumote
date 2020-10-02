6ae961d8b79fae482863ddf3ecefc500c6f97bf8
Shohei Nakagawa <hukusuke1007@gmail.com>
2020-10-02 09:25:40 +0900

    modify search tile

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -25,6 +25,7 @@ class SearchPage extends TabWidgetPage {
       ),
       body: GridView.count(
         crossAxisCount: 2,
+        childAspectRatio: 0.9,
         children:
             useProvider(provider.state.select((UsersState state) => state))
                 .users
@@ -98,18 +99,16 @@ class SearchTile extends HookWidget {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                child: Row(
-                  children: [
-                    Text(
-                      title,
-                      style: const TextStyle(
-                          fontSize: 17, fontWeight: FontWeight.bold),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
                 ),
-              )
+              ),
             ],
           ),
           onTap: onTap,
