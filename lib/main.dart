1a545cdd69683091f61ccda15e36b4535b3a8a23
Shohei Nakagawa <hukusuke1007@gmail.com>
2020-10-03 17:02:30 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bakumote/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/all.dart';
 
 import 'directory.dart' as dir;
@@ -7,7 +8,14 @@ import 'logger.dart' as logger;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  /// 縦固定
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  /// 初期化
   await dir.configure();
   logger.configure();
+
   runApp(ProviderScope(child: App()));
 }
