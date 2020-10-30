d062b9bcbdb6a17e9f8ee987eed81a3573d20251
Shohei Nakagawa <hukusuke1007@gmail.com>
2020-10-30 15:26:06 +0900

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,9 @@
 import 'package:bakumote/app_notifier.dart';
-import 'package:bakumote/l10n/l10n.dart';
 import 'package:bakumote/pages/app_page.dart';
 import 'package:bakumote/providers/navigator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/all.dart';
@@ -20,14 +20,12 @@ class App extends HookWidget {
         primarySwatch: Colors.blue,
       ),
       localizationsDelegates: const [
-        L10n.delegate,
         RefreshLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
+        ...AppLocalizations.localizationsDelegates,
       ],
-      localeResolutionCallback: (deviceLocale, supportedLocales) {
-        return const Locale('ja');
-      },
+      supportedLocales: AppLocalizations.supportedLocales,
       home: useProvider(
                   appNotifierProvider.state.select((AppState state) => state))
               .isLoading
